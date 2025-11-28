d64edaf07261c20a5cf3e6a3441117efdce585e0
Jarocki Eryk <eryk.jarocki@grupawp.pl>
2025-11-28 17:44:36 +0100

    refactor: cleanup in keymaps and options

--- lua/josean/core/options.lua
+++ lua/josean/core/options.lua
@@ -6,39 +6,42 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
 
 opt.wrap = false
 
 -- search settings
-opt.ignorecase = true -- ignore case when searching
+opt.ignorecase = true
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+vim.opt.incsearch = true
 
 opt.cursorline = true
 -- adds different character to show diff -> for diffview plugin
-opt.fillchars:append({ diff = "/" })
+opt.fillchars:append({ diff = " " })
 
 -- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "auto:2" -- show sign column so that text doesn't shift
+--
+-- show sign column so that text doesn't shift
+opt.signcolumn = "auto:2"
 
--- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+-- allow backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start"
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true
+opt.splitbelow = true
 
 opt.foldcolumn = "auto"
-opt.foldmethod = "manual" -- Or "indent", "syntax", etc., depending on your needs
+opt.foldmethod = "manual"
 opt.foldenable = true
 
 -- turn off swapfile
@@ -47,15 +50,12 @@ opt.swapfile = false
 -- vim session for plugin auto-session
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
--- detect changes of file in disc
--- when change occurs run :e
+-- detect changes of file in disc when change occurs run :e
 vim.opt.autoread = true
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
   pattern = "*",
   command = "checktime",
 })
-
----------------------------------------------------------------------------
 
 -- persist undo between open and close editor
 vim.opt.undofile = true
