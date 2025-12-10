708f058ca17d2fa0da2f1c09d66d088ee73a1222
Jarocki Eryk <eryk.jarocki@grupawp.pl>
2025-12-10 12:21:08 +0100

    feat: change layout in merge conflicts in diffview

--- lua/josean/plugins/diffview.lua
+++ lua/josean/plugins/diffview.lua
@@ -83,7 +83,7 @@ return {
         },
         merge_tool = {
           -- Config for conflicted files in diff views during a merge or rebase.
-          layout = "diff4_mixed",
+          layout = "diff3_mixed",
           disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
           winbar_info = true, -- See |diffview-config-view.x.winbar_info|
         },
